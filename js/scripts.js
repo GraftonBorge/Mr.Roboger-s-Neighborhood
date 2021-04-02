570bac9ea885f0fbb02ac70f1cfdfcf6bc059dec
GraftonBorge <graftonmborge@gmail.com>
2021-04-02 14:33:29 -0700

    add setup for UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 // buisness 
-const beepboop = [0,1,2,3,4,5];
-  beepboop[1] = "beep";
-  beepboop[2] = "boop";
-  beepboop[3] = "Won't you be my neighbor?"
+const beepBoopArray = [0,1,2,3,4,5];
+  beepBoopArray[1] = "beep";
+  beepBoopArray[2] = "boop";
+  beepBoopArray[3] = "Won't you be my neighbor?"
+function beepBoop(element){
+ if (element = beepBoopArray[1]){
+   return beepBoopArray[1];
+ }
+}
 
 
 
 // UI logic
-
+$(document).ready(function() {
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $(".input").val();
+    const beepBoop = beepBoop(passage);
+    $("#form").html()
+  
+  });
+});
